fa02fd2d41d5afe2598f626cb077d28b3486525a
qwertangel <qwertangel@users.noreply.github.com>
2020-04-19 21:25:22 -0700

    Update costco.js
    much faster. also gets exact times

--- costco.js
+++ costco.js
@@ -1,8 +1,29 @@
 const puppeteer = require('puppeteer');
+const storeName = 'costco';
+
+
+const getTimes = async (page) => {
+    const response = await page.goto('https://sameday.costco.com/v3/retailers/5/delivery_options?source=web');
+    // found response.. now check if there is time
+    if (response.headers().status == 200) {
+        var costcoTimes = await page.evaluate(() => {
+            return JSON.parse(document.querySelector("body").innerText);ç
+        });
+        const allDays = costcoTimes.service_options.days;
+        const daysFound = allDays.filter(day => {
+            return (day.options && day.options.length > 0)}); // if any element found
+        if (daysFound && daysFound.length > 0) {
+            return daysFound;
+        } else {
+            return [];
+        }
+    }  else {
+        //console.log('unauthorized. so returning null');
+        return null;
+    }
+};
 
 const checkCostcoAvailability = async (user, pass, zip) => {
-
-    const storeName = 'costco';
     var myArgs = process.argv.slice(2);
 
     // get args
@@ -13,38 +34,52 @@ const checkCostcoAvailability = async (user, pass, zip) => {
 
     const profileName = storeName + '-' + user.substring(0, 4) + '-' + zip;
 
+    const puppeteerArgs = [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"'
+    ];
+
     const browser = await puppeteer.launch({
         headless: true,
         userDataDir: profileName,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        ignoreDefaultArgs: ['--enable-automation'],
+        args: puppeteerArgs,
         slowMo: 50,
     });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36');
-    await page.setViewport({ width: 1650, height: 800 });
+    await page.setViewport({ width: 1366, height: 1024 });
 
     try {
 
-        await page.goto('https://sameday.costco.com/store/costco/storefront'), { waitUntil: 'networkidle2' };
-
-        const timeSelector = 'a[href^="/' + storeName + '/info?tab=delivery"]>span';
-        const zipSelector = '#signup-zipcode';
-
-        const navigationOutcome = await checkAnySelector(page, [zipSelector, timeSelector]);
-
-        switch (navigationOutcome) {
-            case zipSelector:
-                await login(page, user, pass, zip);
-            // fall through after login
-            case timeSelector:
-                // check date 
-                checkTime(page, timeSelector);
-                break;
-            default:
-                console.log('Error');
-            // error
+        let foundDays = await getTimes(page);
+        if (!foundDays) { // if not logged in then there won't be any 
+            await login(page, user, pass, zip); // logged in
+            foundDays = await getTimes(page); // days found
         }
 
+        if (foundDays && foundDays.length > 0) {
+            let days  = foundDays.reduce((str, day) => {
+                // get times
+                let times = day.options.reduce((dayStr, timeObj) => {
+                    //console.log(JSON.stringify(times));
+                    dayStr += timeObj.green_window + ', ';
+                    return dayStr;
+                }, day.date + ' : ');
+                //console.log('times are ' + times);
+
+                return (str + ' ::::: ' + times);
+            }, (storeName + ' Times Found: '));
+
+            console.log(days);
+        } else {
+            console.log(storeName + ' time not found');
+        }
     } catch (error) {
         console.log('Error: main - ' + error);
         await page.screenshot({ path: '/tmp/' + profileName + '-' + (new Date().getTime())  + '.png'});
@@ -59,37 +94,18 @@ const checkCostcoAvailability = async (user, pass, zip) => {
     }
 };
 
-async function checkTime(page, timeSelector) {
-    await page.waitForSelector(timeSelector, { visible: true, timeout: 15000 });
-    await clickLinkOrButton(page, timeSelector);
-
-    // const deliveryText = await page.evaluate((timeSelector) => document.querySelector(timeSelector).textContent, timeSelector);
-    // console.log('TimeFound : ' + deliveryText + ' - ' + storeName);
-}
-
 async function login(page, username, password, zip) {
+    await page.goto('https://sameday.costco.com/store/costco/storefront', { waitUntil: 'networkidle2' });
     const zipcodeSelector = '#signup-zipcode';
     await enterText(page, zipcodeSelector, zip + String.fromCharCode(13));
     await enterText(page, '#logonId', username); // user field
     await enterText(page, '#logonPassword', password); // password field
     await page.$$eval("option1", checks => checks.forEach(c => c.checked = true));
     await clickLinkOrButton(page, '#LogonForm > fieldset > div:nth-child(5) > input');
+    const timeSelector = 'a[href^="/' + storeName + '/info?tab=delivery"]>span';
+    await page.waitForSelector(timeSelector, { visible: true, timeout: 30000 })
 }
 
-const checkAnySelector = async (page, selectors) => {
-    const jsHandle = await page.waitForFunction((selectors) => {
-        for (const selector of selectors) {
-            if (document.querySelector(selector) !== null) {
-                return selector;
-            }
-        }
-        return false;
-    }, { visible: true, timeout: 10000 }, selectors);
-
-    const selector = await jsHandle.jsonValue();
-    return selector;
-
-}
 
 async function clickLinkOrButton(frame, selector) {
     await frame.waitForSelector(selector, { visible: true, timeout: 20000 })
@@ -99,46 +115,11 @@ async function clickLinkOrButton(frame, selector) {
 }
 
 async function enterText(page, selector, textToEnter) {
+    //console.log('Entering text:' + textToEnter);
     await page.waitForSelector(selector, { visible: true, timeout: 20000 })
     const textBox = await page.$(selector);
     await textBox.focus();
     await page.keyboard.type(textToEnter);
-}
-
-async function enterTextInFrame(page, frame, selector, textToEnter) {
-    await frame.waitForSelector(selector, { visible: true, timeout: 5000 })
-    const textBox = await frame.$(selector);
-    await textBox.focus();
-    await page.keyboard.type(textToEnter);
-}
-
-
-async function recursiveFindInFrames(inputFrame, selector) {
-    const frames = inputFrame.childFrames();
-    const results = await Promise.all(
-        frames.map(async frame => {
-            const el = await frame.$(selector);
-            if (el) {
-                console.log('found ' + selector + ' in frame ' + frame.name());
-                return el;
-            }
-            if (frame.childFrames().length > 0) {
-                return await recursiveFindInFrames(frame, selector);
-            }
-            return null;
-        })
-    );
-    return results.find(Boolean);
-}
-
-async function findInFrames(page, selector) {
-    const result = await recursiveFindInFrames(page.mainFrame(), selector);
-    if (!result) {
-        throw new Error(
-            `The selector \`${selector}\` could not be found in any child frames.`
-        );
-    }
-    return result;
 }
 
 checkCostcoAvailability();
